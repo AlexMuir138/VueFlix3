5bcbbcfd3a7864007c6a0edd7a808bedfd19f1ff
Alex <muirkat138@gmail.com>
2021-10-22 14:38:33 -0600

    search

---
+++ src/pages/SearchPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <span>
+      This is the search page
+    </span>
+
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  
+  setup(){
+    return {}
+  },
+  components:{}
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
