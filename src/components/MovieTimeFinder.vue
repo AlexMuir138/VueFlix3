22f1eacc0d572531e499bdf5159200b210e5f7b0
Alex <muirkat138@gmail.com>
2021-10-26 12:14:50 -0600

    time function

--- src/components/MovieTimeFinder.vue
+++ src/components/MovieTimeFinder.vue
@@ -10,7 +10,9 @@
       class="form-control bg-white border-0 m-2" 
       placeholder="Search Times"
       >
-      <button class="btn btn-secondary mx-3"></button>
+      <button class="btn btn-info mx-3">
+        <i class="mdi mdi-movie-search text-white f-20"></i>
+      </button>
       </div>
     </form>
   </div>
@@ -19,17 +21,27 @@
 
 <script>
 import { ref } from '@vue/reactivity'
+import { AppState } from '../AppState'
 
 import Pop from "../utils/Pop"
+import { moviesService } from "../services/MoviesService"
+import { onMounted } from "@vue/runtime-core"
+import { logger } from "../utils/Logger"
 export default {
 
   setup(){
     const query = ref('')
+     onMounted(() => {
+      moviesService.getMovies()
+      AppState.searchTime = window.localStorage.getItem('time')
+      query.value = AppState.searchTime
+      logger.log('search time', AppState.searchTime)
+    })
     return {
       query,
       async findMovieTimes(){
         try {
-          
+          await moviesService.searchMovieTimes(query.value)
         } catch (error) {
           Pop.toast(error.message, 'error' )
         }
