3a4b80607c010ae501766209aed9270199841518
Alex <muirkat138@gmail.com>
2021-10-25 20:02:42 -0600

    get some

--- src/services/MoviesService.js
+++ src/services/MoviesService.js
@@ -1,9 +1,17 @@
+import { AppState } from "../AppState"
+import { Movie } from '../models/Movie.js'
+import { logger } from "../utils/Logger"
+
 const moviesApi = axios.create({
   baseURL: 'https://6wh65vbmy7.execute-api.us-west-2.amazonaws.com/test/movies'
 })
 
 class MoviesService {
-
+async get Movies(){
+  const res = await moviesApi.get('')
+  AppState.movies = res.data.movies.map(m => new Movie(m))
+  logger.log(AppState.movies)
+}
 }
 
 export const moviesService = new MoviesService()
